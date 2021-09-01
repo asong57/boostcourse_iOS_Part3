145ffb736b19224a47425a27763d4137e90c769b
asong57 <1998thddk@gmail.com>
2021-09-01 14:26:18 +0900

    테이블 뷰에 동적으로 데이터 추가하기

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let korean: [String] = ["가", "나", "다", "라", "마", "바", "사"," 아", "자", "차","카","타","파","하"]
     
     let english: [String] = ["A","B","C", "D","E","F","G","H","I","J","k","L","N","M", "O","P","Q", "R","S","T","U","R","S","T","U", "V","W","X","Y","Z"]
+    
+    var dates: [Date] = []
+    
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+    
+    @IBAction func touchUpAddButton(_ sender: UIButton){
+        dates.append(Date())
+        
+        self.tableView.reloadSections(IndexSet(2...2), with: UITableView.RowAnimation.automatic)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +44,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             return korean.count
         case 1:
             return english.count
+        case 2:
+            return dates.count
         default:
             return 0
         }
@@ -36,18 +53,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        if indexPath.section < 2 {
+            let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+            cell.textLabel?.text = text
+        }else{
+            cell.textLabel?.text = self.dateFormatter.string(from: self.dates[indexPath.row])
+        }
         
-        let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
-        cell.textLabel?.text = text
+        
+        
         return cell
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return section == 0 ? "한글" : "영어"
+        if section < 2 {
+            return section == 0 ? "한글" : "영어"
+        }
+        return nil
     }
 }
 
