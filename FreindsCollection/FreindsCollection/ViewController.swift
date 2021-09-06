fc638038da795ec923f83cf488fb8862fa73e274
asong57 <1998thddk@gmail.com>
2021-09-06 14:39:28 +0900

    컬렉션뷰에 동적으로 데이터 추가하기

--- FreindsCollection/FreindsCollection/ViewController.swift
+++ FreindsCollection/FreindsCollection/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     var numberOfCell: Int = 10
     let cellIdentifier: String = "cell"
@@ -21,6 +21,10 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.numberOfCell += 1
+        collectionView.reloadData()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
