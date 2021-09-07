3ca4fe45d36efda343fc87a422c477dafade201e
asong57 <1998thddk@gmail.com>
2021-09-07 09:25:52 +0900

    셀 커스터마이징

---
+++ FreindsCollection/FreindsCollection/FriendCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendCollectionViewCell.swift
+//  FreindsCollection
+//
+//  Created by asong on 2021/09/07.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var nameAgeLAbel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+}
--- FreindsCollection/FreindsCollection/ViewController.swift
+++ FreindsCollection/FreindsCollection/ViewController.swift
@@ -9,25 +9,46 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    var numberOfCell: Int = 10
+    //var numberOfCell: Int = 10
     let cellIdentifier: String = "cell"
+    var friends: [Friend] = []
+    @IBOutlet weak var collectionView: UICollectionView!
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.numberOfCell
+        return self.friends.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath)
+        let cell: FriendCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath) as! FriendCollectionViewCell
+        
+        let friend: Friend = self.friends[indexPath.item]
+        
+        cell.nameAgeLAbel.text = friend.nameAndAge
+        cell.addressLabel.text = friend.fullAddress
+        
         return cell
     }
-    
+    /*
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.numberOfCell += 1
         collectionView.reloadData()
-    }
+    }*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "friends") else{
+            return
+        }
+        
+        do{
+            self.friends = try jsonDecoder.decode([Friend].self, from: dataAsset.data)
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        self.collectionView.reloadData()
         // Do any additional setup after loading the view.
     }
 
